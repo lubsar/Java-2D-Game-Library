07aa0fae31f216e27d88dc61b45ccb9668ef4654
-global <lubomir.hlavko@gmail.com>
2018-09-26 11:38:47 +0200

    updated file structure, documented Core.java, BasicCore.java and fixed fps and tps counting logic (updated once a second

--- src/svk/lubsar/j2dgl/core/BasicCore.java
+++ src/svk/lubsar/j2dgl/core/BasicCore.java
@@ -1,67 +1,132 @@
-package svk.sglubos.oengine.lib.core;
+package svk.lubsar.j2dgl.core;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.Timer;
 
-import svk.sglubos.oengine.lib.utils.debug.DebugStringBuilder;
-import svk.sglubos.oengine.lib.utils.debug.MessageHandler;
+import svk.lubsar.j2dgl.utils.debug.DebugStringBuilder;
+import svk.lubsar.j2dgl.utils.debug.MessageHandler;
 
+/**
+ * Basic implementation of the {@link Core} class with game loop.
+ * <p>
+ * The game loop implementation uses {@link javax.swing.Timer}.
+ * Update cycle is determined by constructor parameter updatesDelay.
+ * Update and render is synchronized, so both tick() and render functions are called every game loop cycle (in such order).
+ * Inherit this class to use its functionality.
+ * <p>
+ * Debug information is passed to {@link svk.lubsar.j2dgl.utils.debug.MessageHandler MessageHandler}.
+ * 
+ * @see Core
+ */
 public abstract class BasicCore extends Core {
-	public static final int FPS_UNLIMITED = -1;
-	
+	/**
+	 * Javax Timer used for timing of the update and render
+	 */
 	protected Timer timer;
 	
+	/**
+	 * Controls whether game loop generates debug output
+	 */
 	private boolean debug;
-	private int updatesDelay;
+	
+	/**
+	 * Delay in-between updates in milliseconds
+	 */
+	private int updatesDelayMs;
+	
+	/**
+	 * Cumulated number of rendered frames per second.
+	 */
 	private int fps;
+	
+	/**
+	 * Cumulated number of updates per second.
+	 */
 	private int ticks;
 	
-	public BasicCore(int updatesDelay, boolean debug) {
-		this.updatesDelay = updatesDelay;
+	/**
+	 * @param updatesDelayMs delay between updates in milliseconds
+	 * @param debug determines whether debug information is generated
+	 * 
+	 * @throws RuntimeException if updates delay is less than 1
+	 * 
+	 * @see BasicCore
+	 */
+	public BasicCore(int updatesDelayMs, boolean debug) {
+		if(updatesDelayMs < 1) {
+			throw new RuntimeException("Update delay must be higher than 0");
+		}
+		this.updatesDelayMs = updatesDelayMs;
 		this.debug = debug;
 	}
 
+	/**
+	 * @return delay between updates in milliseconds
+	 */
 	protected int getUpdatesDelayMs() {
-		return updatesDelay;
+		return updatesDelayMs;
 	}
 	
+	/**
+	 * @return <code><b>true</b></code> if debug information is generated <code><b>false</b></code> otherwise
+	 */
 	protected boolean isDebug() {
 		return debug;
 	}
 	
+	/**
+	 * Set flag whether debug information should be generated or not
+	 * @param debug
+	 */
 	protected void setDebug(boolean debug) {
 		this.debug = debug;
 	}
-		
+	
+	/**
+	 * Starts game loop. {@link #init() init()} is called before the {@link javax.swing.Timer} is set up.
+	 * 
+	 * @throws RuntimeException if game loop is already running
+	 * 
+	 * @see #stop()
+	 */
 	@Override
 	protected void start() {
+		if(running) {
+			throw new RuntimeException("Game loop is already running");
+		}
+		
 		running = true;
 		init();
 		
-		timer = new Timer(updatesDelay, new ActionListener() {
+		timer = new Timer(updatesDelayMs, new ActionListener() {
 			long lastTimeNano = System.nanoTime();
 			long lastTimeDebugOutput = System.currentTimeMillis();
+			
+			int ticksCumulated = 0;
+			int fpsCumulated = 0;
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				long now = System.nanoTime();
-				tick((now - lastTimeNano) / (updatesDelay * 1000000));
+				tick((now - lastTimeNano) / (updatesDelayMs * 1000000));
 				
-				ticks++;
+				ticksCumulated++;
 				
 				render();
-				fps++;
+				fpsCumulated++;
 				
 				if((System.currentTimeMillis() - lastTimeDebugOutput) >= 1000){
+					BasicCore.this.ticks = ticksCumulated;
+					BasicCore.this.fps = fpsCumulated;
 					if(debug) {
-						MessageHandler.printMessage(MessageHandler.INFO, "ticks: " + ticks + " fps: " + fps + "delta: " + ((double)(now - lastTimeNano) / (updatesDelay * 1000000)));
+						MessageHandler.printMessage(MessageHandler.INFO, "ticks: " + ticks + " fps: " + fps + "delta: " + ((double)(now - lastTimeNano) / (updatesDelayMs * 1000000)));
 					}
 					
 					lastTimeDebugOutput += 1000;
-					fps = 0;
-					ticks = 0;
+					ticksCumulated = 0;
+					fpsCumulated = 0;
 				}
 				
 				lastTimeNano = now;
@@ -73,18 +138,34 @@ public abstract class BasicCore extends Core {
 		timer.start();
 	}
 	
+	/**
+	 * Stops the game loop.
+	 * 
+	 * @throws RuntimeException if game loop is not running
+	 * 
+	 * @see #start()
+	 */
 	@Override
 	protected void stop() {
+	 if(!running) {
+		 throw new RuntimeException("Game loop is not running");
+	 }
 	 timer.stop();
 	 stopped();
 	 running = false;
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	protected int getFPS() {
 		return fps;
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	protected int getTPS() {
 		return ticks;
@@ -97,7 +178,7 @@ public abstract class BasicCore extends Core {
 		ret.increaseLayer();
 		ret.appendln(super.toString());
 		ret.append("debug", debug);
-		ret.append("updatesDelayMs", updatesDelay);
+		ret.append("updatesDelayMs", updatesDelayMs);
 		ret.append("tps", ticks);
 		ret.append("fps", fps);
 		ret.append(timer, "Timer");
--- src/svk/lubsar/j2dgl/core/Core.java
+++ src/svk/lubsar/j2dgl/core/Core.java
@@ -1,19 +1,84 @@
-package svk.sglubos.oengine.lib.core;
+package svk.lubsar.j2dgl.core;
 
-import svk.sglubos.oengine.lib.utils.debug.DebugStringBuilder;
+import svk.lubsar.j2dgl.utils.debug.*;
 
+/**
+ * Abstract class intended for elemental logic of the application such as starting/stopping 
+ * and the entry points for updating and rendering.
+ * 
+ * @see BasicCore
+ * @see MultiThreadCore
+ */
 public abstract class Core {
+	/**
+	 * Keeps state whether the logic is running or not.
+	 * 
+	 * @see #isRunning()
+	 * @see #start()
+	 * @see #stop()
+	 */
 	protected volatile boolean running;
 	
+	/**
+	 * Starts elemental logic. Call to {@link #init() init()} function should be included (first).
+	 * @see #ini()
+	 */
 	protected abstract void start();
+	
+	/**
+	 * Stops elemental logic. Call to {@link #stopped() stoped()} should be included (last). 
+	 * Sets running to false.
+	 * 
+	 * @see #stopped()
+	 */
 	protected abstract void stop();
+	
+	/**
+	 * Entry point for initialization.
+	 * 
+	 * @see #start()
+	 */
 	protected abstract void init();
+	
+	/**
+	 * Entry point for updating.
+	 * 
+	 * @param delta coefficient of passed time and desired delay in-between ticks
+	 */
 	protected abstract void tick(double delta);
+	
+	/**
+	 * Entry point for rendering.
+	 */
 	protected abstract void render();
+	
+	/**
+	 * Callback after the logic was stopped.
+	 * 
+	 * @see #stop()
+	 */
 	protected abstract void stopped();
+	
+	/**
+	 * Returns the number of rendered frames in the timespan of 1 second.
+	 * 
+	 * @return FPS
+	 */
 	protected abstract int getFPS();
+	
+	/**
+	 * Returns the number of updates in the timespan of 1 second.
+	 * 
+	 * @return TPS 
+	 */
 	protected abstract int getTPS();
 	
+	/**
+	 * @return <b>{@code true}</b> if logic is running, <b>{@code false}</b> otherwise
+	 * 
+	 * @see #start()
+	 * @see #stop()
+	 */
 	public boolean isRunning() {
 		return running;
 	}
